2c054c7ba0c8562f28ef5efc7fd6a5a5b898c458
53 <gt_vengeance@yahoo.co.jp>
2019-09-17 02:45:55 +0900

    Implement lottie animation for roppongivue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld />
+    <RoppongiVue />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import RoppongiVue from "./components/RoppongiVue.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    RoppongiVue
   }
 };
 </script>
---
+++ src/components/RoppongiVue.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <lottie-animation
+      path="roppongivueanime.json"
+      :loop="false"
+      :height="null"
+      :width="null"
+      class="cover"
+      @AnimControl="setAnimController"
+      ref="lottie"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.cover {
+  object-fit: cover;
+  height: 98vh;
+  width: 100%;
+  box-sizing: border-box;
+}
+</style>
+
+<script>
+export default {
+  name: "RoppongiVue",
+  data() {
+    return {
+      isFinished: false
+    };
+  },
+  watch: {
+    isFinished(_new) {
+      if (_new) {
+        console.log("Animation End");
+      }
+    }
+  },
+  methods: {
+    setAnimController(anim) {
+      let animloop = () => {
+        if (anim.isPaused) {
+          this.isFinished = true;
+          return;
+        }
+        window.requestAnimationFrame(animloop);
+      };
+      animloop();
+    }
+  }
+};
+</script>
